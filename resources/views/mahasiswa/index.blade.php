c073aa374d18fb19d7a15832c3fb61850619b7a7
abiwahyu <delas639@gmail.com>
2020-07-21 18:24:54 +0700

    pert 11 : Membuat Halaman Template

---
+++ resources/views/mahasiswa/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('title','Halaman mahasiswa')
+
+@section('bread1','mahasiswa')
+@section('bread2','daftar mahasiswa')
+
+@section('content')
+    <p>Udah lumayan rumit nich</p>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('mahasiswa', 'MahasiswaController@index');
+Route::get('mahasiswa', 'MahasiswaController@index')->name('mahasiswa');
